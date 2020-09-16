885655aa13f0c08ebc5ea839825be9234a8ad173
Am4nzi <gvs.0027@gmail.com>
2020-09-16 16:11:57 +0100

    wrap FormQuestionName component in govuk-fieldset

--- src/views/FormQuestionName.vue
+++ src/views/FormQuestionName.vue
@@ -1,10 +1,17 @@
 <template>
-  <div class="govuk-form-group">
-    <GovukLabel form-question-label="What is your name?" />
-    <div>
-      <input v-model="inputValue" class="govuk-input" autocomplete="on" />
+  <fieldset class="govuk-fieldset">
+    <GovukFieldsetLegend legend-text="What is your name?" />
+    <GovukErrorMessage
+      v-if="true"
+      error-message="Dummy Message"
+    />
+    <div class="govuk-form-group">
+      <GovukLabel form-question-label="Full name" />
+      <div>
+        <input v-model="inputValue" class="govuk-input" autocomplete="on" />
+      </div>
     </div>
-  </div>
+  </fieldset>
 </template>
 
 <script>
