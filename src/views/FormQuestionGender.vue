0a113bdb70864a3bc58993e926d209eab48e0b50
Am4nzi <gvs.0027@gmail.com>
2020-09-16 16:01:21 +0100

    add content to FormQuestionGender componenet

--- src/views/FormQuestionGender.vue
+++ src/views/FormQuestionGender.vue
@@ -1,4 +1,29 @@
 <template>
+  <div class="govuk-form-group">
+    <GovukLabel form-question-label="What is your gender?" />
+
+    <div>
+      <div class="govuk-radios govuk-radios--inline">
+        <div class="govuk-radios__item">
+          <input
+              v-model="inputValue"
+              class="govuk-radios__input"
+              type="radio"
+              value="Male"
+          />
+          <label class="govuk-label govuk-radios__label"> Male </label>
+        </div>
+        <div class="govuk-radios__item">
+          <input
+              v-model="inputValue"
+              class="govuk-radios__input"
+              type="radio"
+              value="Female"
+          />
+          <label class="govuk-label govuk-radios__label"> Female </label>
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
