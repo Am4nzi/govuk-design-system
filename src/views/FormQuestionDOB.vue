8fce6c0641218bfd757d73e9b2e7f58f1078e9ab
Am4nzi <gvs.0027@gmail.com>
2020-09-16 16:08:46 +0100

    add content to FormQuestionDOB.vue

---
+++ src/views/FormQuestionDOB.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div id="dob-hint" class="govuk-hint">
+      "For example, 31 3 1980"
+    </div>
+    <div class="govuk-date-input">
+      <div class="govuk-date-input__item">
+        <div class="govuk-form-group">
+          <label class="govuk-label govuk-date-input__label">
+            Day
+          </label>
+          <input
+              v-model="dateDay"
+              class="govuk-input govuk-date-input__input govuk-input--width-2"
+              type="text"
+              autocomplete="on"
+              pattern="[0-9]*"
+              inputmode="numeric"
+              maxlength="2"
+          />
+        </div>
+      </div>
+      <div class="govuk-date-input__item">
+        <div class="govuk-form-group">
+          <label class="govuk-label govuk-date-input__label">
+            Month
+          </label>
+          <input
+              v-model="dateMonth"
+              class="govuk-input govuk-date-input__input govuk-input--width-2"
+              type="text"
+              autocomplete="on"
+              pattern="[0-9]*"
+              inputmode="numeric"
+              maxlength="2"
+          />
+        </div>
+      </div>
+      <div class="govuk-date-input__item">
+        <div class="govuk-form-group">
+          <label class="govuk-label govuk-date-input__label">
+            Year
+          </label>
+          <input
+              v-model="dateYear"
+              class="govuk-input govuk-date-input__input govuk-input--width-4"
+              type="text"
+              autocomplete="on"
+              pattern="[0-9]*"
+              inputmode="numeric"
+              maxlength="4"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FormQuestionDOB",
+};
+</script>
