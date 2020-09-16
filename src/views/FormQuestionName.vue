380daf6297f8a06dccbf82c9f9a9302afc41648b
Am4nzi <gvs.0027@gmail.com>
2020-09-16 16:05:23 +0100

    add content to FormQuestionName view

--- src/views/FormQuestionName.vue
+++ src/views/FormQuestionName.vue
@@ -1,8 +1,13 @@
 <template>
+  <div class="govuk-form-group">
+    <GovukLabel form-question-label="What is your name?" />
+    <div>
+      <input v-model="inputValue" class="govuk-input" autocomplete="on" />
+    </div>
+  </div>
 </template>
 
 <script>
-
 export default {
   name: "FormQuestionName",
 };
