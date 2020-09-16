370e8d5e5a17ececcac84f101cbc5f1dd14e5d7a
Am4nzi <gvs.0027@gmail.com>
2020-09-16 16:38:13 +0100

    wrap FormQuestionGender in govuk-fieldset

--- src/views/FormQuestionGender.vue
+++ src/views/FormQuestionGender.vue
@@ -1,34 +1,42 @@
 <template>
-  <div class="govuk-form-group">
-    <GovukLabel form-question-label="What is your gender?" />
-
-    <div>
-      <div class="govuk-radios govuk-radios--inline">
-        <div class="govuk-radios__item">
-          <input
+  <fieldset class="govuk-fieldset">
+    <GovukFieldsetLegend legend-text="What is your name?" />
+    <GovukErrorMessage v-if="true" error-message="Dummy Message" />
+    <div class="govuk-form-group">
+      <div>
+        <div class="govuk-radios govuk-radios--inline">
+          <div class="govuk-radios__item">
+            <input
               v-model="inputValue"
               class="govuk-radios__input"
               type="radio"
               value="Male"
-          />
-          <label class="govuk-label govuk-radios__label"> Male </label>
-        </div>
-        <div class="govuk-radios__item">
-          <input
+            />
+            <label class="govuk-label govuk-radios__label"> Male </label>
+          </div>
+          <div class="govuk-radios__item">
+            <input
               v-model="inputValue"
               class="govuk-radios__input"
               type="radio"
               value="Female"
-          />
-          <label class="govuk-label govuk-radios__label"> Female </label>
+            />
+            <label class="govuk-label govuk-radios__label"> Female </label>
+          </div>
         </div>
       </div>
     </div>
+  </fieldset>
 </template>
 
 <script>
-
+import GovukErrorMessage from "../components/GovukErrorMessage";
+import GovukFieldsetLegend from "../components/GovukFieldsetLegend";
 export default {
   name: "FormQuestionGender",
+  components: {
+    GovukErrorMessage,
+    GovukFieldsetLegend,
+  },
 };
 </script>
