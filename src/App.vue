344cf79deeaf823d99a071f51e331ec745e05454
Am4nzi <gvs.0027@gmail.com>
2020-09-16 18:18:22 +0100

    merge Layoute.vue into App.vue & reflect changes in router

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,26 @@
 <template>
   <div id="app">
-    <router-view />
+    <div>
+      <GovukHeader />
+      <GovukBackLink :link-action="navigateToPreviousRoute" />
+      <div class="govuk-width-container">
+        <main class="govuk-main-wrapper" id="main-content" role="main">
+          <div
+              :class="{
+            'govuk-form-group': true,
+            'govuk-form-group--error': true,
+          }"
+          >
+            <router-view />
+          </div>
+          <GovukButton
+              button-text="Continue"
+              :button-action="navigateToNextRoute"
+          />
+        </main>
+      </div>
+      <GovukFooter />
+    </div>
   </div>
 </template>
 
@@ -8,12 +28,53 @@
 import Vue from "vue";
 import Vuelidate from 'vuelidate'
 import getAndSetFormValues from "./mixins/getAndSetFormValues";
+import GovukHeader from "./components/GovukHeader";
+import GovukBackLink from "./components/GovukBackLink";
+import GovukFooter from "./components/GovukFooter";
+import GovukButton from "./components/GovukButton";
 
 Vue.use(Vuelidate)
 Vue.mixin(getAndSetFormValues);
 
 export default {
   name: "App",
+  components: {
+    GovukHeader,
+    GovukBackLink,
+    GovukFooter,
+    GovukButton
+  },
+  methods: {
+    getPreviousRoute() {
+      const { name } = this.$route;
+      switch (name) {
+        case "Summary List":
+          return "Gender";
+        case "Gender":
+          return "Date of birth";
+        case "Date of birth":
+          return "Name";
+      }
+    },
+    getNextRoute() {
+      const { name } = this.$route;
+      switch (name) {
+        case "Name":
+          return "Date of birth";
+        case "Date of birth":
+          return "Gender";
+        case "Gender":
+          return "Summary List";
+      }
+    },
+    navigateToPreviousRoute() {
+      this.$store.dispatch("updateFormErrorIsActive", false);
+      this.$router.push({ name: this.getPreviousRoute() });
+    },
+    navigateToNextRoute() {
+      this.$router.push({ name: this.getNextRoute() });
+    },
+  },
 };
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Layout from "../views/Layout.vue";
 import SummaryList from "../views/SummaryList.vue";
 import FormQuestionName from "../views/FormQuestionName.vue";
 import FormQuestionDOB from "../views/FormQuestionDOB.vue";
@@ -11,30 +10,29 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: "/",
-    component: Layout,
-    children: [
-          {
-            path: "/name",
-            name: "Name",
-            component: FormQuestionName,
-          },
-          {
-            path: "/date-of-birth",
-            name: "Date of birth",
-            component: FormQuestionDOB,
-          },
-          {
-            path: "/gender",
-            name: 'Gender',
-            component: FormQuestionGender,
-          },
-        ],
-      },
-      {
-        path: "/summary-list",
-        name: "Summary List",
-        component: SummaryList,
-      },
+    redirect: { name: "Name" },
+  },
+  {
+    path: "/name",
+    name: "Name",
+    component: FormQuestionName,
+  },
+  {
+    path: "/date-of-birth",
+    name: "Date of birth",
+    component: FormQuestionDOB,
+  },
+  {
+    path: "/gender",
+    name: "Gender",
+    component: FormQuestionGender,
+  },
+
+  {
+    path: "/summary-list",
+    name: "Summary List",
+    component: SummaryList,
+  },
 ];
 
 const router = new VueRouter({
